0ee7e9e9a38f9f4178e436249ed2770631d7a1fd
jovination <jovination791@gmail.com>
2023-12-15 23:30:18 +0300

    coding function of toggle to showoff MENU

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import './header.css';
 
 const Header = () => {
+    /* toggle menu */
     const[Toggle, showMenu] = useState(false);
+
+
   return (
     <header className="header">
         <nav className="nav container">
 
             <a href="index.html" className="nav__logo">MeSkills</a>
               
-         <div className="nav__menu">
+         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
             <ul className="nav__list grid">
 
                 <li className="nav__item">
@@ -53,10 +56,10 @@ const Header = () => {
                     </a>
                 </li>
 
-               <i className="uil uil-times nav__close"></i>
+               <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
             </ul>
         </div>
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
         <i class="uil uil-apps"></i>       
          </div>
 
