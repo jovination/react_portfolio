004ec818830a7add8a0d42e12310f00467641b92
jovination <jovination791@gmail.com>
2023-12-15 19:09:03 +0300

    adding styling css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div>
+      <Header />
     </div>
   );
 }
