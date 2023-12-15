e13f6c80a8a59c26f03f34436aeaf9ca1156ef76
jovination <jovination791@gmail.com>
2023-12-15 20:09:57 +0300

    working on navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <nav className="nav container">
+            <a href="index.html" className="nav__logo">Portfolio</a>
+        </nav>
+        <div className="nav__menu">
+            <ul className="nav__list grid">
+
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+
+                        </i>
+                    </a>
+                </li>
+
+
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+                            
+                        </i>
+                    </a>
+                </li>
+
+
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+                            
+                        </i>
+                    </a>
+                </li>
+
+
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+                            
+                        </i>
+                    </a>
+                </li>
+
+
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+                            
+                        </i>
+                    </a>
+                </li>
+
+
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+                            
+                        </i>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+    </header>
   )
 }
 
