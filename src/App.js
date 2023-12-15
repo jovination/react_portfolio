0ad0cf25c13560b8def7d6193ce350c6c57358af
jovination <jovination791@gmail.com>
2023-12-15 23:42:15 +0300

    workig on hero section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <main className="main">
+        <Home />
+      </main>
+
     </div>
   );
 }
