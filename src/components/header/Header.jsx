1e050b8e96074bcac4df2c4abb641935eef7f64e
jovination <jovination791@gmail.com>
2023-12-15 20:34:59 +0300

    adding icons to navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,69 +5,57 @@ const Header = () => {
   return (
     <header className="header">
         <nav className="nav container">
-            <a href="index.html" className="nav__logo">Portfolio</a>
+
+            <a href="index.html" className="nav__logo">MeSkills</a>
               
-        <div className="nav__menu">
+         <div className="nav__menu">
             <ul className="nav__list grid">
 
                 <li className="nav__item">
-                    <a href="" className="nav__link">
-                        <i className="uil uil- nav__icon">
-
-                        </i>
+                    <a href="#home" className="nav__link">
+                        <i className="uil uil-estate nav__icon"></i> Home
                     </a>
                 </li>
 
 
                 <li className="nav__item">
-                    <a href="" className="nav__link">
-                        <i className="uil uil- nav__icon">
-                            
-                        </i>
+                    <a href="#about" className="nav__link">
+                        <i className="uil uil-user nav__icon"></i> About
                     </a>
                 </li>
 
 
                 <li className="nav__item">
-                    <a href="" className="nav__link">
-                        <i className="uil uil- nav__icon">
-                            
-                        </i>
+                    <a href="#skills" className="nav__link">
+                        <i className="uil uil-file-alt nav__icon"></i> Skills
                     </a>
                 </li>
 
 
                 <li className="nav__item">
-                    <a href="" className="nav__link">
-                        <i className="uil uil- nav__icon">
-                            
-                        </i>
+                    <a href="#services" className="nav__link">
+                        <i className="uil uil-briefcase-alt nav__icon"></i> Services
                     </a>
                 </li>
 
 
                 <li className="nav__item">
-                    <a href="" className="nav__link">
-                        <i className="uil uil- nav__icon">
-                            
-                        </i>
+                    <a href="#portfolio" className="nav__link">
+                        <i className="uil uil-scenery nav__icon"></i> Portfolio
                     </a>
                 </li>
 
 
                 <li className="nav__item">
-                    <a href="" className="nav__link">
-                        <i className="uil uil- nav__icon">
-                            
-                        </i>
+                    <a href="#contact" className="nav__link">
+                        <i className="uil uil-message nav__icon"></i> Contact
                     </a>
                 </li>
 
             </ul>
         </div>
-        </nav>
-      
 
+        </nav>
     </header>
   )
 }
